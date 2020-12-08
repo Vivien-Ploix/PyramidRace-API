aa4ca89ad3373699b019988fca6c9ca720829372
Vivien-Ploix <vivien.ploix@gmail.com>
2020-12-08 21:18:28 +0100

    add game_histories table, model, relations, and index show create destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 
 
 Rails.application.routes.draw do
-
+    resources :game_histories, only: %w[show index create destroy]
     resources :games, only: %w[show index create destroy]
     resources :users, only: %w[show index]
     devise_for :users,
