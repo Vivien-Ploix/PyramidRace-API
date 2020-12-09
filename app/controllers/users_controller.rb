c3c495622c70726b0daaee2839023e2e5029fdc2
Vivien-Ploix <vivien.ploix@gmail.com>
2020-12-09 19:14:50 +0100

    add possibility to fetch user games

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,16 @@ class UsersController < ApplicationController
   end
 
   def show
-    render_jsonapi_response(@user)
+    @user = User.find(params[:id])
+    render json: @user
   end
+
+  def user_games
+    @user = User.find(params[:id])
+    @user_games = @user.player1_games + @user.player2_games
+    @user_games = @user_games.sort_by(&:updated_at).reverse
+    render json: @user_games
+  end 
+
 
 end
